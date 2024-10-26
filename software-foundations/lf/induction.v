c7c5ccb9a9d79a1453a6a7e525eb6ef93bdc0a1d
hyperb0rean <jewellin02@gmail.com>
2024-10-26 15:49:27 +0300

    induction

--- software-foundations/lf/induction.v
+++ software-foundations/lf/induction.v
@@ -1,1 +1,18 @@
 From LF Require Export basics.
+
+Fixpoint double (n:nat) :=
+  match n with
+  | O => O
+  | S n' => S (S (double n'))
+  end.
+
+Lemma double_plus : forall n, double n = n + n .
+Proof.
+  induction n.
+  - rewrite -> plus_0_n.
+    reflexivity.
+  - symmetry. simpl. 
+    rewrite <- plus_n_Sm.
+    rewrite <- IHn.
+    reflexivity.
+    Qed.
