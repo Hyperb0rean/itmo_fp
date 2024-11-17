8d5f78fd7186a099f5b6839af9d5026013c457f4
hyperb0rean <jewellin02@gmail.com>
2024-11-18 02:33:39 +0300

    some palindrom shenanigans

--- project-euler/largest_palindrome.v
+++ project-euler/largest_palindrome.v
@@ -1,154 +1,67 @@
-From LF Require Export basics.
+Require Import Coq.Lists.List.
+Require Import Coq.Arith.PeanoNat.
+Require Import Coq.Arith.Wf_nat.
+Import ListNotations.
 
-Module Palindrome.
-Inductive num: Type :=
-| Nil
-| Zero (_: num)
-| One (_: num)
-| Two (_: num)
-| Three (_: num)
-| Four (_: num)
-| Five (_: num)
-| Six (_: num)
-| Seven (_: num)
-| Eight (_: num)
-| Nine (_: num)
-.
+Require Import Wellfounded.
 
-Declare Scope palindrome_scope.
-Open Scope palindrome_scope.
+Search lt.
+Search le.
 
-Notation "0" := Zero.
-Notation "1" := One.
-Notation "2" := Two.
-Notation "3" := Three.
-Notation "4" := Four.
-Notation "5" := Five.
-Notation "6" := Six.
-Notation "7" := Seven.
-Notation "8" := Eight.
-Notation "9" := Nine.
+Lemma digits_oblig (n m : nat) (p : n <> 0) (q : m > 1) : n / m < n.
+Proof.
+  apply Nat.div_lt.
+  - destruct n. contradiction. apply Nat.lt_0_succ.
+  - apply q.
+Qed.
 
-Notation " [ x1 , .. , x2 ] " := (x2 .. ( x1 Nil) .. ).
+Lemma not_null (n: nat) : n<>0.
+Admitted.
+Lemma greater_1 (m: nat) : m > 1.
+Admitted.
 
-Fixpoint succ (d : num) : num :=
-  match d with
-  | Nil => One Nil
-  | Zero d => One d
-  | One d => Two d
-  | Two d => Three d
-  | Three d => Four d
-  | Four d => Five d
-  | Five d => Six d
-  | Six d => Seven d
-  | Seven d => Eight d
-  | Eight d => Nine d
-  | Nine d => Zero (succ d)
-  end. 
 
-Notation " ++ x1 " := (succ x1) (at level 70).
+Definition digits (n : nat) : list nat :=
+  Fix (lt_wf) (fun _ => list nat)
+      (fun (n : nat) (digits : forall n', (n' < n) -> list nat) =>
+        match n with
+        | 0 => []
+        | _ => digits (n / 10) (digits_oblig n 10 (not_null n) (greater_1 10))  ++ [n mod 10]
+        end) 
+    n.
 
-Example test_succ: ++ [ 1, 2 ] =  [ 1, 3 ].
-Proof. reflexivity. Qed.
-
-Fixpoint reverse (d d' : num) :=
-  match d with
-  | Nil => d'
-  | Zero d => reverse d (Zero d')
-  | One d => reverse d (One d')
-  | Two d => reverse d (Two d')
-  | Three d => reverse d (Three d')
-  | Four d => reverse d (Four d')
-  | Five d => reverse d (Five d')
-  | Six d => reverse d (Six d')
-  | Seven d => reverse d (Seven d')
-  | Eight d => reverse d (Eight d')
-  | Nine d => reverse d (Nine d')
+Fixpoint list_beq {A: Type} (eq: A -> A -> bool) (x y : list A) : bool :=
+  match x, y with
+  | [], [] => true                 
+  | _, [] => false                 
+  | [], _ => false                 
+  | h1 :: t1, h2 :: t2 =>         
+      eq h1 h2 && list_beq eq t1 t2  
   end.
 
-Fixpoint eq_num (a b : num) : bool :=
-    match a, b with
-    | Nil, Nil => true
-    | Zero a, Zero b => (eq_num a b)
-    | One a, One b => (eq_num a b)
-    | Two a, Two b => (eq_num a b)
-    | Three a, Three b => (eq_num a b)
-    | Four a, Four b => (eq_num a b)
-    | Five a, Five b => (eq_num a b)
-    | Six a, Six b => (eq_num a b)
-    | Seven a, Seven b => (eq_num a b)
-    | Eight a, Eight b => (eq_num a b)
-    | Nine a, Nine b => (eq_num a b)
-    | _, _ => false
-    end.
+Fixpoint eqb (n m : nat) : bool :=
+  match n, m with
+  | O, O => true
+  | O, S _ => false
+  | S _, O => false
+  | S n', S m' => eqb n' m' 
+  end.
 
-Notation " x1 =? x2 " := (eq_num x1 x2) (at level 70).
+Definition is_palindrome (n : nat) : bool :=
+  let ds := digits n in
+  list_beq eqb ds (rev ds).
 
-Example test_eq_num1: [ 1, 2 ] =? [ 1, 2 ] = true.
-Proof. reflexivity. Qed.
-Example test_eq_num2: [ 1, 2 , 4 ] =? [ 1, 3, 4 ] = false.
-Proof. reflexivity. Qed.
+Fixpoint find_max (i j max_prod upper_limit lower_limit : nat) : nat :=
+max_prod.
 
-Definition is_palindrom (a : num) := 
-    eq_num a ( reverse a Nil ).
+Definition largest_palindrome (n : nat) : nat :=
+  let upper_limit := (10 ^ n) - 1 in
+  let lower_limit := 10 ^ (n - 1) in
+  find_max upper_limit upper_limit 0 upper_limit lower_limit.
 
-
-Example test_is_palindrom1: is_palindrom [ 1, 2, 1 ] = true.
-Proof. reflexivity. Qed.
-Example test_is_palindrom2: is_palindrom [ 1, 2, 3, 2, 1 ] = true.
-Proof. reflexivity. Qed.
-Example test_is_palindrom3: is_palindrom [ 1, 2, 3, 1 ] = false.
-Proof. reflexivity. Qed.
-
-Fixpoint succ_m (a: num) (m: nat) : num :=
-    match m with
-    | O => a
-    | S m' => (succ_m (succ a) m')
-    end. 
-
-
-Notation " m + a " := (succ_m a m).
-
-Example test_succ_m: 1 + [1, 3, 5] = [1, 3, 6].
-Proof. simpl. reflexivity. Qed.
-Example test_succ_m2: 9 + [1, 3, 5] = [1, 4, 4].
-Proof. simpl. reflexivity. Qed.
-Example test_succ_m3: 19 + [1, 3, 5] = [1, 5, 4].
+Compute largest_palindrome 2.
+Example test_2: largest_palindrome 2 = 9009.
 Proof. simpl. reflexivity. Qed.
 
-Fixpoint dec_to_nat (a: num) : nat :=
-    match a with
-    | Nil => O
-    | Zero a' => mult 10 (dec_to_nat a')
-    | One a' => (plus 1 (mult 10 (dec_to_nat a')))
-    | Two a' => (plus 2 (mult 10 (dec_to_nat a')))
-    | Three a' => (plus 3 (mult 10 (dec_to_nat a')))
-    | Four a' => (plus 4 (mult 10 (dec_to_nat a')))
-    | Five a' => (plus 5 (mult 10 (dec_to_nat a')))
-    | Six a' => (plus 6 (mult 10 (dec_to_nat a')))
-    | Seven a' => (plus 7 (mult 10 (dec_to_nat a')))
-    | Eight a' => (plus 8 (mult 10 (dec_to_nat a')))
-    | Nine a' => (plus 9 (mult 10 (dec_to_nat a')))
-    end.
-
-Definition mod_10 (a :num) : num :=
-    match a with
-    | Nil => Nil
-    | One _ => (One Nil)
-    | Two _ => (Two Nil)
-    | Three _ => (Three Nil)
-    | Four _ => (Four Nil)
-    | Five _ => (Five Nil)
-    | Six _ => (Six Nil)
-    | Seven _ => (Seven Nil)
-    | Eight _ => (Eight Nil)
-    | Nine _ => (Nine Nil)
-    | Zero _ => (Zero Nil)
-    end.
-
-
-Example test_mod_10: mod_10 [1, 2, 3] = [3].
-Proof. simpl. reflexivity. Qed. 
-
-
-End Palindrome.
+Example test_3: largest_palindrome 3 = 906609.
+Proof. simpl. reflexivity. Qed.
