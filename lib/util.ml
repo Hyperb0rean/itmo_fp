c4b1c4988e1c21e43ba48cbaa97acb28281c9a70
hyperb0rean <jewellin02@gmail.com>
2025-02-28 21:09:13 +0300

    clear code

--- lib/util.ml
+++
@@ -1,7 +0,0 @@
-(* open Libcoq *)
-
-(* let rec nat_to_int (n : nat) : int =
-  match n with O -> 0 | S m -> 1 + nat_to_int m
-
-let rec int_to_nat (n : int) : nat =
-  if n <= 0 then O else S (int_to_nat (n - 1)) *)
